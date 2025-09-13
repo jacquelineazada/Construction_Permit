af557a389b795bef17ae5c9dc499e02f9b9a4867
Jacqueline <jakilo.azada0507@gmail.com>
2025-09-13 14:53:31 +0800

    changes

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,15 +4,20 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
 
   routes: [
+    // Main Route
     {
       path: "/",
       name: "login",
       component: () => import("@/pages/login.vue"),
     },
-    {
-      path: "/",
-      name: "",
-    },
+
+    // Route for Building Permit
+
+    // Route for Locational Clearance
+
+    // Route for Compliance Monitoring
+
+    // Route for Occupancy Permit
   ],
 });
 
